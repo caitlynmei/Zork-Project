1036591f8462de6a1edb4f56eeb21beae088e9b3
cmei <cmei@cmeisp3.bayviewglen.ca>
2017-05-02 13:13:43 -0400

    I uploaded the command actions file and added a third option (can use 3 words) in the command.java file.

--- Zork/src/com/bayviewglen/zork/Command.java
+++ Zork/src/com/bayviewglen/zork/Command.java
@@ -17,7 +17,8 @@ package com.bayviewglen.zork;
 class Command{   
 	
 	private String commandWord;   
-	private String secondWord;    
+	private String secondWord; 
+	private String thirdWord;
 	
 	/**    
 	 * * Create a command object. First and second word must be supplied, but    
@@ -29,6 +30,14 @@ class Command{
 		commandWord = firstWord;       
 		this.secondWord = secondWord;  
 		}   
+	
+	// CM
+	public Command(String firstWord, String secondWord, String thirdWord){
+		commandWord = firstWord;
+		this.secondWord = secondWord;
+		this.thirdWord = thirdWord;
+	}
+	
 	
 	/** Return the command word (the first word) of this command. 
 	  **If the command was not understood, the result is null. */   
@@ -44,17 +53,25 @@ class Command{
 		 return secondWord;  
 		 }   
 	 
+	 // @return the third word of this command. Returns null if there was no third word.
+	 public String getThirdWord(){
+		 return thirdWord;
+	 }
+	 
 	  /**Return true if this command was not understood.    
 	   */  
-	  public boolean isUnknown()  
-	  {      
+	  public boolean isUnknown(){      
 		  return (commandWord == null);   
-		  } 
+	  } 
 	  
 	  /**Return true if the command has a second word. */   
-	  public boolean hasSecondWord()  
-	  {     
+	  public boolean hasSecondWord(){     
 		  return (secondWord != null);  
-		  }
 	  }
 
+	  // @return true if the command has a third word.
+	  public boolean hasThirdWord(){
+		  return (thirdWord != null);
+	  }
+
+}
