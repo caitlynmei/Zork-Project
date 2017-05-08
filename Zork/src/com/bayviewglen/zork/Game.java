9dc3cd3e5a759108b319fe7aa578d28049341f5e
dmiret <dmiret@dmiretsp3.bayviewglen.ca>
2017-05-08 13:38:18 -0400

    update to rooms.dat

--- Zork/src/com/bayviewglen/zork/Game.java
+++ Zork/src/com/bayviewglen/zork/Game.java
@@ -25,7 +25,7 @@ import java.util.Scanner;
  *  commands that the parser returns.
  */
 
-class Game 
+class Game //*
 {
     private Parser parser;
     private Room currentRoom;
@@ -97,7 +97,7 @@ class Game
      */
     public Game() {
         try {
-			initRooms("data/Rooms.dat");
+			initRooms("data/rooms.dat");
 			currentRoom = masterRoomMap.get("ROOM_1");
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
@@ -138,7 +138,7 @@ class Game
         System.out.println("Get ready to some action!");
         System.out.println("Type 'help' if you need help.");
         System.out.println();
-        System.out.println(currentRoom.longDescription());
+        System.out.println(currentRoom.longDescription()); //*
     }
 
     /**  
