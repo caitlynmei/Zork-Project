dcf1eb08f1658ec6b95ed15a11b3047020ffc9ee
ggerges <ggerges@ggergessp3.bayviewglen.ca>
2017-05-10 14:49:19 -0400

    fix

--- Zork/src/com/bayviewglen/zork/Items/Tools.java
+++ Zork/src/com/bayviewglen/zork/Items/Tools.java
@@ -33,24 +33,3 @@ public class Tools implements Items{
 
 
 
-public class Lighting implements Items extends Tools{
-
-	@Override
-	public void Name(String Name) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public boolean equals(Items item) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public void Increment() {
-		// TODO Auto-generated method stub
-		
-	} 
-	
-}
