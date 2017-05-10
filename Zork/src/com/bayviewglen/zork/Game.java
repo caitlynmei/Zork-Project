07367b201ecb4323d2023508289aadcf440e93a0
dmiret <dmiret@dmiretsp3.bayviewglen.ca>
2017-05-10 15:22:30 -0400

    Trying to debug

--- Zork/src/com/bayviewglen/zork/Game.java
+++ Zork/src/com/bayviewglen/zork/Game.java
@@ -97,7 +97,7 @@ class Game //*
      */
     public Game() {
         try {
-			initRooms("data/rooms.dat");
+			initRooms("rooms.dat");
 			currentRoom = masterRoomMap.get("ROOM_1");
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
