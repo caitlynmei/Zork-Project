da2c53ee3fefb3f4fe7112e280512d9ad6819b97
ggerges <ggerges@ggergessp3.bayviewglen.ca>
2017-05-10 14:55:59 -0400

    new Tool classes

---
+++ Zork/src/com/bayviewglen/zork/Items/Break.java
@@ -0,0 +1,27 @@
+package com.bayviewglen.zork.Items;
+
+public class Break extends Tools implements Items {
+
+	public Break() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void Name(String Name) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean equals(Items item) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void Increment() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- Zork/src/com/bayviewglen/zork/Items/Weapons.java
+++ Zork/src/com/bayviewglen/zork/Items/Weapons.java
@@ -1,6 +1,6 @@
 package com.bayviewglen.zork.Items;
 
-public abstract class Weapons implements Items {
+public class Weapons implements Items  {
 
 	private int Damage;
 	private int Durability;
@@ -22,8 +22,26 @@ public abstract class Weapons implements Items {
 	public int getwei(){
 		return weight;
 	}
+	
 	public String getname(){
 		return Name;
+	}
+	
+	
+	@Override
+	public void Name(String Name) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public boolean equals(Items item) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	@Override
+	public void Increment() {
+		// TODO Auto-generated method stub
+		
 	}
 	
 	
