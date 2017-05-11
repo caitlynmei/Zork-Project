816e73b0114c4ce95b8a562e8ec6577e2508a1ee
dmiret <dmiret@dmiretsp3.bayviewglen.ca>
2017-05-11 12:26:54 -0400

    THE GAME WORKS YAY

--- Zork/src/com/bayviewglen/zork/Game.java
+++ Zork/src/com/bayviewglen/zork/Game.java
@@ -97,7 +97,7 @@ class Game
      */
     public Game() {
         try {
-			initRooms("data/rooms.dat");
+			initRooms("data/levels/level0.dat");
 			currentRoom = masterRoomMap.get("ROOM_1");
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
