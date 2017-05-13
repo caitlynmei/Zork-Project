6fbd37e6ea388934ad3856f0618597904d0ea64c
cmei <cmei@cmeisp3.bayviewglen.ca>
2017-05-12 20:28:49 -0400

    Edited intro dialoge and "help" command.

--- Zork/src/com/bayviewglen/zork/CommandWords.java
+++ Zork/src/com/bayviewglen/zork/CommandWords.java
@@ -8,50 +8,44 @@ package com.bayviewglen.zork;
  * * This class is part of the "Zork" game.
  * */
 
-class CommandWords{ 
-	
-	// a constant array that holds all valid first command words (word1)
-	private static final String validCommands[] = {"go", "quit", "help", "eat", "take", "drop", "inventory",
-											
-												  };
-	// 	   "north", "south", "east", "west", "N", "S", "E", "W" word2 - put in game.java - CM
-	
-	/**  
-	 *  Constructor - initialize the command words. 
-	 **/  
-	public CommandWords() 
-		{       
-			// nothing to do at the moment...    
-		}  
-	
-	
-	/**  
-	 ** Check whether a given String is a valid command word.     
-	 ** Return true if it is, false if it isn't.     
-	 **/   
-	public boolean isCommand(String aString)   
-		{        
-			for(int i = 0; i < validCommands.length; i++)    
-				{          
-					if(validCommands[i].equals(aString))     
-						return true;    
-				}   // if we get here, the string was not found in the commands      
-			return false;   
-			}   
-	
-	/* 
-	 * * Print all valid commands to System.out.     
-	 * */   
-	public void showAll(){       
-			for(int i = 0; i < validCommands.length; i++){     
-					
-				System.out.print(validCommands[i] + ", ");    
-						
-				}  
-			
-			System.out.println();  
-		}
+class CommandWords {
 
-	
+	// Constant String holding all valid first command words (word1)
+	private static final String validCommands[] = { "go", "quit", "eat", "take", "drop", "inventory", "help"};
+	// "north", "south", "east", "west", "N", "S", "E", "W" word2 - put in
+	// game.java - CM
+
+	/**
+	 * Constructor - initialize the command words.
+	 **/
+	public CommandWords() {
+		// nothing to do at the moment...
 	}
 
+	/**
+	 ** Check whether a given String is a valid command word. Return true if it
+	 * is, false if it isn't.
+	 **/
+	public boolean isCommand(String aString) {
+		for (int i = 0; i < validCommands.length; i++) {
+			if (validCommands[i].equals(aString))
+				return true;
+		} // if we get here, the string was not found in the commands
+		return false;
+	}
+
+	// showAll Method: prints all valid commands
+	public void showAll() {
+		for (int i = 0; i < validCommands.length; i++) {
+			if (i == validCommands.length - 1) {
+				System.out.print(validCommands[i]);
+			} else if (i == 0){
+				System.out.print("> " + validCommands[i] + ", ");
+			} else {
+				System.out.print(validCommands[i] + ", ");
+			}
+		}
+		System.out.println();
+	}
+
+}
