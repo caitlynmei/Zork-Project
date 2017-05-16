74ce2341139e8af392c97c743a13ff5f9367a633
ggerges <ggerges@ggergessp3.bayviewglen.ca>
2017-05-16 15:18:13 -0400

    Dialogue for intro in progress. Pushing for Caitlyn's use.

--- Zork/src/com/bayviewglen/zork/Dialogue.java
+++ Zork/src/com/bayviewglen/zork/Dialogue.java
@@ -16,13 +16,15 @@ public class Dialogue {
 	public static void level0() throws InterruptedException{
 		
 		 System.out.println("\nMysterious figure :");
-		 thread.sleep(500);
 		 System.out.println("Look who decided to finally wake up");
 					thread.sleep(1500);
 					
 					System.out.println("\n\nYou think about what to say. You think to yourself\n");
+					thread.sleep(2000);
 					System.out.println("\t\"Who are you?\" - \"Where am I?\"\n" );
+					thread.sleep(2000);
 					System.out.println("What do you say?");
+					System.out.print(">");
 					String temp = "";
 					boolean valid = false;
 					while (!valid){
@@ -33,31 +35,35 @@ public class Dialogue {
 					}
 					else{
 						System.out.println("That doesn't appear in your thoughts...");
+						System.out.print(">");
 					}
 						
 					}
 					
 					if(temp.equals("WHO ARE YOU?") || temp.equals("WHO ARE YOU")){
-						System.out.println("You say:\nWho are you?\n");
-						thread.sleep(500);
-						System.out.println("Mysterious Figure:\n");
+						System.out.println("\nYou say:\nWho are you?\n");
+						thread.sleep(2000);
+						System.out.println("\nMysterious Figure:");
 						System.out.println("You don't Remember? You don't remember the hell you put me through?");
-						thread.sleep(500);
+						thread.sleep(2500);
 						System.out.println("Well, that will make this more enjoyable. I might feel a bit guilty after though...");
-						thread.sleep(500);
+						thread.sleep(2000);
 						
 						
 						
 						
 					}else if( temp.equals("WHERE AM I?") || temp.equals("WHERE AM I")){
 						
-						System.out.println("You say:\nWhere am I?");
-						System.out.println("Mysterious Figure:\n");
+						System.out.println("\nYou say:\nWhere am I?");
+						thread.sleep(2000);
+						System.out.println("\nMysterious Figure:\n");
 						System.out.println("You are in a place where you will finally get what you deserve.");
-						
-						System.out.println("You say:\nWho are you?\n");
+						thread.sleep(3000);
+						System.out.println("\nYou say:\nWho are you?\n");
+						thread.sleep(1000);
 						System.out.println("Mysterious Figure:\n");
 						System.out.println("You don't Remember? You don't remember the hell you put me through?");
+						thread.sleep(2500);
 						System.out.println("Well, that will make this more enjoyable. I might feel a bit guilty after though...");
 						
 					}
