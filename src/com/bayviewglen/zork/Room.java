2885bcf4608463d97614895fd30ab828a5cbc42b
dmiret <dmiret@dmiretsp3.bayviewglen.ca>
2017-05-24 14:09:19 -0400

    Equipables

--- src/com/bayviewglen/zork/Room.java
+++ src/com/bayviewglen/zork/Room.java
@@ -23,8 +23,6 @@ class Room
 	private String roomName;
     private String description;
     private HashMap<String, Room> exits;        // stores exits of this room.
-    private String items;
-    private String Enemies;
 
     /**
      * Create a room described "description". Initially, it has no exits.
@@ -41,8 +39,6 @@ class Room
     	roomName = "DEFAULT ROOM";
     	description = "DEFAULT DESCRIPTION";
     	exits = new HashMap<String, Room>();
-    	items = "NO ITEMS";
-    	Enemies = "NO ENEMIES";
 	}
 
     public void setExit(char direction, Room r) throws Exception{
