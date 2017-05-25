70cf46586c074b4a08e94ba821d0d497acae0de8
jtran <jtran@jetransp3.bayviewglen.ca>
2017-05-25 14:09:38 -0400

    Dialogue class

---
+++ src/com/bayviewglen/zork/DialogueLevel3.java
@@ -0,0 +1,9 @@
+package com.bayviewglen.zork;
+
+public class DialogueLevel3 {
+
+	public DialogueLevel3() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/com/bayviewglen/zork/DialogueLevel4.java
@@ -0,0 +1,9 @@
+package com.bayviewglen.zork;
+
+public class DialogueLevel4 {
+
+	public DialogueLevel4() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
