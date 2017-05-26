710c3bad1546ebba6ab8b9b440a0f5c2e2f7eec2
dmiret <dmiret@dmiretsp3.bayviewglen.ca>
2017-05-26 10:44:04 -0400

    Some more stuff

--- src/com/bayviewglen/zork/characters/Characters.java
+++ src/com/bayviewglen/zork/characters/Characters.java
@@ -2,4 +2,11 @@ package com.bayviewglen.zork.characters;
 
 public interface Characters {
 
+	public String name = "Characters";
+	
+	public String getName();
+	
+	public String getDescription();
+	
+	public boolean equals(Characters Characters);
 }
--- src/com/bayviewglen/zork/characters/Enemies.java
+++ src/com/bayviewglen/zork/characters/Enemies.java
@@ -1,5 +1,25 @@
 package com.bayviewglen.zork.characters;
 
 public class Enemies implements Characters {
+	
+	
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getDescription() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean equals(Characters Characters) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
 }
