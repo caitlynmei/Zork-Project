d7bcad0e4895d56cce2fb1640fc531d57f1cb789
ggerges <ggerges@ggergessp3.bayviewglen.ca>
2017-05-31 14:11:28 -0400

    level 4 fixes --> first puzzle

--- src/com/bayviewglen/zork/DialogueLevel4.java
+++ src/com/bayviewglen/zork/DialogueLevel4.java
@@ -1,9 +1,64 @@
 package com.bayviewglen.zork;
 
+import java.util.Scanner;
+
 public class DialogueLevel4 {
 
 	public DialogueLevel4() {
 		// TODO Auto-generated constructor stub
 	}
-
+	
+	public void LevelIntro() throws InterruptedException{ //level start
+		
+		System.out.println("You wake up and look around you");
+		Thread.sleep(5000);
+		System.out.println("*sniff*");
+		Thread.sleep(5000);
+		System.out.println("You smell the scent of ashes");
+		Thread.sleep(5000);
+		System.out.println("You look around to that you are in a cave. Is this a Volcano?");
+		Thread.sleep(5000);
+		
+	}
+	
+	public void personOne() throws InterruptedException{
+		Scanner keyboard = new Scanner(System.in); 
+		System.out.println("You see a cloaked figure in the corner");
+		Thread.sleep(5000);
+		System.out.println("\n\nYou think about what to do. You think to yourself\n");
+		Thread.sleep(2000);
+		System.out.println("\t\"Approach them\" - \"Ignore\"\n" );
+		Thread.sleep(2000);
+		System.out.println("What do you do?");
+		System.out.print(">");
+		String temp = "";
+		boolean valid = false;
+		while (!valid){
+		
+		temp = keyboard.nextLine().toUpperCase();
+		if(temp.equals("APPROACH") || temp.equals("APPROACH THEM") || temp.equals("IGNORE")){
+			valid = true;
+		}else if(temp.equals("HELP")){
+			System.out.println("\nType what you are thinking.");
+		}
+		else{
+			System.out.println("That doesn't appear in your thoughts...");
+			System.out.print(">");
+		}
+			
+		
+		if(temp.equals("APPROACH")|| temp.equals("APPROACH THEM")){
+		
+			//what happens when they approach them
+		
+		
+		} else{
+			System.out.println("You ignore them. Which direction would you like to go?");
+		}
+	
+		}
+		
+		}
+	
+	
 }
